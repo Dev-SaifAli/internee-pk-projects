70f76af36b8d8d3f1ee217d66469d9b327ef79ed
SAIF ALI RABBANI <saifu100@outlook.com>
2026-02-14 21:46:33 +0500

    get and initialize elements

---
+++ Drag and Drop File Uploader/js/script.js
@@ -0,0 +1,13 @@
+const dropZone = document.getElementById("dropZone");
+const fileInput = document.getElementById("uploadFile");
+const errorMessage = document.getElementById("errorMessage");
+const successMessage = document.getElementById("successMessage");
+const previewSection = document.getElementById("previewSection");
+const previewGrid = document.getElementById("previewGrid");
+const progressContainer = document.getElementById("progressContainer");
+const progressBar = document.getElementById("progressBar");
+const progressText = document.getElementById("progressText");
+const clickLink = document.querySelector(".click-link");
+const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+
+
